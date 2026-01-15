b6bdcd014d3a2109155fce82825b794c5bafc9f8
MauroRaya <maurorayabr@gmail.com>
2026-01-15 09:26:26 -0300

    feat: import csv library and add main function and entrypoint

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import boto3
+import csv
 
 
 def list_user_policy_names(client: boto3.client, username: str) -> list[str]:
@@ -27,3 +28,32 @@ def list_attached_group_policy_names(client: boto3.client, group_name: str) -> l
     response = client.list_attached_group_policies(GroupName=group_name)
     policies = response['AttachedPolicies']
     return [policy['PolicyName'] for policy in policies]
+
+
+def main():
+    client = boto3.client('iam')
+
+    with open('status_reports.csv', 'r') as f:
+        reader = csv.DictReader(f)
+
+        for _, line in enumerate(reader):
+            username = line['user']
+            policies = set()
+
+            try:
+                policies.update(list_user_policy_names(client, username))
+                policies.update(list_attached_user_policy_names(client, username))
+
+                group_names = list_group_names_for_user(client, username)
+
+                for group_name in group_names:
+                    policies.update(list_group_policy_names(client, group_name))
+                    policies.update(list_attached_group_policy_names(client, group_name))
+
+                print(f'{username} - {policies}')
+            except:
+                continue
+
+
+if __name__ == '__main__':
+    main()
